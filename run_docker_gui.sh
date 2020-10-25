d2fabf616745352846d168893fcaddf01200a3d2
Timur Akhtyamov <rentgeny05@gmail.com>
2020-10-25 18:46:35 +0300

    Host master connection fixed

--- run_docker_gui.sh
+++ run_docker_gui.sh
@@ -31,6 +31,8 @@ docker run --rm -it \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --env="XAUTHORITY=$XAUTH" \
     --volume="$XAUTH:$XAUTH" \
+    --env="ROS_IP=172.17.0.1" \
+    --env="ROS_MASTER_URI=http://172.17.0.1:11311" \
     --net=host \
     --runtime=nvidia \
     --privileged \
