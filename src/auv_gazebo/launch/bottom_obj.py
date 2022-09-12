9841e9577804114528e021293b5550d0611de82f
khosta77 <puwerfulpants@gmail.com>
2022-09-12 12:18:42 +0300

    Добавлено случайное положение пингера у столба и в баках

--- src/auv_gazebo/launch/bottom_obj.py
+++ src/auv_gazebo/launch/bottom_obj.py
@@ -1,5 +1,12 @@
 import random
 
+fl_1 = [random.uniform(24.0,43.15), random.uniform(15.0,24.05)]
+fl_2 = [random.uniform(24.0,43.15), random.uniform(15.0,24.05)]
+pf = random.sample([fl_1, fl_2], 1)
+
+bc = [random.uniform(24.0,43.15), random.uniform(15.0,24.05)]
+pb = [[bc[0] + 1.5, bc[0] + 3.4, bc[0] + 5.75], bc[1]]
+
 my_file = open("bottom_objects.launch", "w")
 my_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
               "<launch>\n\n"
@@ -39,8 +46,8 @@ my_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
               
               "    <include file=\"$(find spawn_sdf)/launch/spawn_sdf.launch\">\n"
               "        <arg name=\"robot_name\" value=\"yellow_flare_1\" />\n"
-             f"        <arg name=\"x\" value=\"{random.uniform(24.0,43.15)}\" />\n"
-             f"        <arg name=\"y\" value=\"{random.uniform(15.0,24.05)}\" />\n"
+             f"        <arg name=\"x\" value=\"{fl_1[0]}\" />\n"
+             f"        <arg name=\"y\" value=\"{fl_1[1]}\" />\n"
               "        <arg name=\"z\" value=\"3.2\" />\n"
               "        <arg name=\"roll\" value=\"0\"/>\n"
               "        <arg name=\"pitch\" value=\"0\"/>\n"
@@ -50,19 +57,41 @@ my_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
               
               "    <include file=\"$(find spawn_sdf)/launch/spawn_sdf.launch\">\n"
               "        <arg name=\"robot_name\" value=\"yellow_flare_2\" />\n"
-             f"        <arg name=\"x\" value=\"{random.uniform(24.0,43.15)}\" />\n"
-             f"        <arg name=\"y\" value=\"{random.uniform(15.0,24.05)}\" />\n"
+             f"        <arg name=\"x\" value=\"{fl_2[0]}\" />\n"
+             f"        <arg name=\"y\" value=\"{fl_2[1]}\" />\n"
               "        <arg name=\"z\" value=\"3.2\" />\n"
               "        <arg name=\"roll\" value=\"0\"/>\n"
               "        <arg name=\"pitch\" value=\"0\"/>\n"
               "        <arg name=\"yaw\" value=\"0.0\" />\n"
               "        <arg name=\"sdf_robot_file\" value=\"$(find spawn_sdf)/models/yellow_flare/model.sdf\" />\n"
               "    </include>\n\n"
-              
+
+              "    <include file=\"$(find spawn_sdf)/launch/spawn_sdf.launch\">\n"
+              "        <arg name=\"robot_name\" value=\"pinger_buckets\" />\n"
+             f"        <arg name=\"x\" value=\"{random.sample(pb[0], 1)}\" />\n"
+             f"        <arg name=\"y\" value=\"{pb[1]}\" />\n"
+              "        <arg name=\"z\" value=\"1.3\" />\n"
+              "        <arg name=\"roll\" value=\"0\"/>\n"
+              "        <arg name=\"pitch\" value=\"0\"/>\n"
+              "        <arg name=\"yaw\" value=\"0.0\" />\n"
+              "        <arg name=\"sdf_robot_file\" value=\"$(find spawn_sdf)/models/pinger/model.sdf\" />\n"
+              "    </include>\n\n"
+
+              "    <include file=\"$(find spawn_sdf)/launch/spawn_sdf.launch\">\n"
+              "        <arg name=\"robot_name\" value=\"pinger_flare\" />\n"
+             f"        <arg name=\"x\" value=\"{pf[0] + 0.2}\" />\n"
+             f"        <arg name=\"y\" value=\"{pf[1] + 0.2}\" />\n"
+              "        <arg name=\"z\" value=\"1.3\" />\n"
+              "        <arg name=\"roll\" value=\"0\"/>\n"
+              "        <arg name=\"pitch\" value=\"0\"/>\n"
+              "        <arg name=\"yaw\" value=\"0.0\" />\n"
+              "        <arg name=\"sdf_robot_file\" value=\"$(find spawn_sdf)/models/pinger/model.sdf\" />\n"
+              "    </include>\n\n"
+           
               "    <include file=\"$(find spawn_sdf)/launch/spawn_sdf.launch\">\n"
               "        <arg name=\"robot_name\" value=\"buckets\" />\n"
-             f"        <arg name=\"x\" value=\"{random.uniform(24.0,43.15)}\" />\n"
-             f"        <arg name=\"y\" value=\"{random.uniform(15.0,24.05)}\" />\n"
+             f"        <arg name=\"x\" value=\"{bc[0]}\" />\n"
+             f"        <arg name=\"y\" value=\"{bc[1]}\" />\n"
               "        <arg name=\"z\" value=\"1.21\" />\n"
               "        <arg name=\"roll\" value=\"0\"/>\n"
               "        <arg name=\"pitch\" value=\"0\"/>\n"
